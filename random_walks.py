469d29b71f74d5c5033d18255bb15f5bc3255733
khmkii <james_mccombie@hotmail.com>
2018-04-05 13:54:32 +0100

    Edit show_paths to allow x-axis or y-axis to display time

--- random_walks.py
+++ random_walks.py
@@ -2,11 +2,15 @@ import matplotlib.pyplot as plt
 import random
 
 
-def shift_y_axis():
+def shift_axis(shift):
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
-    ax.spines['left'].set_position(('data', 0.0))
-    ax.yaxis.set_label_coords(0.0, 1.0)
+    if shift == 'x':
+        ax.spines['bottom'].set_position('center')
+        ax.xaxis.set_label_coords(1.05, 0.5)
+    else:
+        ax.spines['left'].set_position(('data', 0.0))
+        ax.yaxis.set_label_coords(0.0, 1.0)
 
 
 class OneDimRandomWalk:
@@ -15,6 +19,8 @@ class OneDimRandomWalk:
 
         Initialise with a time scale as an positive integer, and a number
         of walkers
+
+        default settings are walkers=2, for time=20 time steps
 
         Two instance methods:
 
@@ -28,7 +34,7 @@ class OneDimRandomWalk:
 
     directions = [1, -1]
 
-    def __init__(self, time, walkers):
+    def __init__(self, time=20, walkers=2):
         self.time_coordinates = [t for t in range(0, time + 1)]
         self.walkers = walkers
 
@@ -41,15 +47,31 @@ class OneDimRandomWalk:
             self.paths.append(path)
         return self.paths
 
-    def show_paths(self):
+    def show_paths(self, orientation='y'):
+        """
+            defaults to show time along the y-axis, parameter orientation='x'
+            will display time along the x-axis
+        """
+
         tracks = self.walk()
-        x_max = max([max(x) for x in tracks])
-        x_min = min([min(x) for x in tracks])
-        shift_y_axis()
-        for track in tracks:
-            plt.plot(track, self.time_coordinates)
-        plt.xlabel("distance (x)")
-        plt.ylabel("time")
-        plt.xlim(x_min - 1, x_max + 1)
-        plt.ylim(0, self.time_coordinates[-1] + 1)
-        plt.show()
+        track_max = max([max(x) for x in tracks])
+        track_min = min([min(x) for x in tracks])
+
+        if orientation == 'x':
+            shift_axis('x')
+            for track in tracks:
+                plt.plot(self.time_coordinates, track)
+            plt.xlabel("time")
+            plt.ylabel("distance (y)")
+            plt.xlim(0, self.time_coordinates[-1] + 1)
+            plt.ylim(track_min - 1, track_max + 1)
+            plt.show()
+        else:
+            shift_axis('y')
+            for track in tracks:
+                plt.plot(track, self.time_coordinates)
+            plt.xlabel("distance (x)")
+            plt.ylabel("time")
+            plt.xlim(track_min - 1, track_max + 1)
+            plt.ylim(0, self.time_coordinates[-1] + 1)
+            plt.show()
